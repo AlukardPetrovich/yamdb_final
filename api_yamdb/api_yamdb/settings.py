48398e3514ba2e76eb518733f9a52ba40eb6acc7
Galitsin Nikolay <ngalitsin@gmail.com>
2022-05-21 13:26:24 +0300

    Deploy test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = False
-ALLOWED_HOSTS = ['51.250.105.18']
+ALLOWED_HOSTS = ['84.252.137.86']
 
 
 INSTALLED_APPS = [
