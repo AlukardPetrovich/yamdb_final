336581334c5b402d2ec159f9e8b706689390c2b1
Galitsin Nikolay <ngalitsin@gmail.com>
2022-05-22 14:15:55 +0300

    ALLOWED_HOSTS in secrets

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = False
-ALLOWED_HOSTS = os.getenv('SECRET_KEY').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 INSTALLED_APPS = [
